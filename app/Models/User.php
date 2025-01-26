f149b95b4932a04a937c3505a3bb767c4d070594
ciocanucarolinadev <91370151+ciocanucarolinadev@users.noreply.github.com>
2025-01-26 15:34:03 +0200

    #dev: Updated Job table columns.

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
@@ -45,4 +46,10 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    // Relate to job listing 
+    public function jobListings(): HasMany {
+        return $this->hasMany(JobListing::class);
+    }
+
 }
