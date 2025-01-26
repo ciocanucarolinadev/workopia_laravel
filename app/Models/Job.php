f5766d533f0e17ece4e5a2f9c63b0144fe9fc91e
ciocanucarolinadev <91370151+ciocanucarolinadev@users.noreply.github.com>
2025-01-26 16:22:01 +0200

    #dev: Database seeders.

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,11 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Job extends Model
 {
+    use HasFactory, Notifiable;
+
     protected $table = 'job_listings';
 
     protected $fillable = [
--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Job>
@@ -17,7 +18,25 @@ class JobFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'title' => $this->faker->jobTitle(),
+            'description' => $this->faker->paragraphs(2, true),
+            'salary' => $this->faker->numberBetween(40000, 120000),
+            'tags' => implode(', ', $this->faker->words(3)),
+            'job_type' => $this->faker->randomElement(['Full-Time', 'Part-Time', 'Contract']),
+            'remote' => $this->faker->boolean(),
+            'requirements' => $this->faker->sentences(3, true),
+            'benefits' => $this->faker->sentences(2, true),
+            'address' => $this->faker->streetAddress(),
+            'city' => $this->faker->city(),
+            'state' => $this->faker->state(),
+            'zipcode' => $this->faker->postcode(),
+            'contact_email' => $this->faker->safeEmail(),
+            'contact_phone' => $this->faker->phoneNumber(),
+            'company_name' => $this->faker->company(),
+            'company_description' => $this->faker->paragraphs(2, true),
+            'company_logo' => $this->faker->imageUrl(100, 100, 'business', true, 'logo'),
+            'company_website' => $this->faker->url(),
         ];
     }
 }
