265ebab11c034ff08d9eea56565e0d91c88c1aa5
ciocanucarolinadev <91370151+ciocanucarolinadev@users.noreply.github.com>
2025-02-12 07:56:14 +0200

    Updated create job function.

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -57,7 +57,7 @@
 
         <x-inputs.text-area id="company_description" name="company_description" label="Company Description" placeholder="Enter Company Description" />
 
-        <x-inputs.text id='company_website' name='company_website' label="Company Website" placeholder="Enter Company Website" />
+        <x-inputs.text id='company_website' type='url' name='company_website' label="Company Website" placeholder="Enter Company Website" />
 
         <x-inputs.text id='contact_phone' name='contact_phone' label="Contact Phone" placeholder="Enter Contact Phone" />
 
