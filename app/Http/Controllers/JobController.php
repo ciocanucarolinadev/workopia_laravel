53ccb140a689e8560dd9b8fdc67fcd501e1b741c
ciocanucarolinadev <ciocanu.carolina.dev@gmail.com>
2025-03-17 13:40:48 +0200

    Upload files.

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -31,6 +31,8 @@ class JobController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
+        // dd($request->file('company_logo'));
+        // php artisan storage:link
         $validatedData = $request->validate([
             'title' => 'required|string|max:255',
             'description' => 'required|string',
@@ -55,6 +57,16 @@ class JobController extends Controller
         // Hardcoded user ID
         $validatedData['user_id'] = 1;
 
+        // Check for image
+        if($request->hasFile('company_logo')) {
+            // Store the file and get path
+            $path = $request->file('company_logo')->store('logos', 'public');
+
+            // Add path to validated data
+            $validatedData['company_logo'] = $path;
+        }
+
+        // Submit to database
         Job::create($validatedData);
 
         return redirect()->route('jobs.index')->with('success', 'Job listing created successfully!');
