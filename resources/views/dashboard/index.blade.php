9654ab089cef73b25de7ed5840da5ff957a648e6
ciocanucarolinadev <ciocanu.carolina.dev@gmail.com>
2025-04-23 18:52:29 +0300

    Upload user avatar.

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -6,12 +6,20 @@
                 Profile Info
             </h3>
 
+            @if($user->avatar)
+            <div class="mt-2 flex justify-center">
+                <img src="{{ asset('storage/' . $user->avatar) }}" alt="{{ $user->name }}"
+                    class="w-32 h-32 object-cover rounded-full">
+            </div>
+            @endif
+
             <form method="POST" action="{{ route('profile.update') }}" enctype="multipart/form-data">
                 @csrf
                 @method('PUT')
 
                 <x-inputs.text id="name" name="name" label="Name" value="{{ $user->name }}" />
                 <x-inputs.text id="email" name="email" label="Email" value="{{ $user->email }}" />
+                <x-inputs.file id="avatar" name='avatar' label="Upload Avatar" />
 
                 <button type="submit"
                     class="w-full bg-green-500 hover:bg-green-600 focus:outline-none text-white px-4 py-2 border-rounded">
