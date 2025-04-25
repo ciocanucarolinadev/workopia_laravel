ac52c341fd7713fd0f380a2da03758ec41621f4a
ciocanucarolinadev <ciocanu.carolina.dev@gmail.com>
2025-04-25 09:57:18 +0300

    Add pagination.

---
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -0,0 +1,31 @@
+@if($paginator->hasPages())
+<nav class="flex justify-center" role="navigation">
+    <!-- Prev link -->
+    @if($pagination->onFisrtPage())
+    <span class="px-4 py-2 bg-gray-300 text-gray-500 rounded-l-lg">Previous</span>
+    @else
+    <a href="{{ $pagination->previousPageUrl() }}"
+        class="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-l-lg">Previous</a>
+    @endif
+
+    <!-- Pagination Elements -->
+    @foreach($elements as $element)
+    <!-- Three Dots Separatoe -->
+    @if(is_string($element))
+    <span class="px-4 py-2 bg-gray-300 text-gray-500">{{ $element }}</span>
+    @endif
+    <!-- Array Of Links -->
+    @if(is_array($element))
+    @foreach($element as $page => $url)
+    @if($page == $pagination->currentPage())
+    <span class="px-4 py-2 bg-blue-500 text-white">{{ $page }}</span>
+    @else
+    <a href="{{ $url }}" class="px-4 py-2 bg-gray-200 text-gray-700 hover:bg-blug-600 hover:text-white">{{ $page }}</a>
+    @endif
+    @endforeach
+    @endif
+    @endforeach
+</nav>
+@else
+
+@endif
