67f4edd1a21c7bb0879edcc9698842eb63bb9d64
ciocanucarolinadev <ciocanu.carolina.dev@gmail.com>
2025-07-17 16:24:38 +0300

    #dev: Fixed pagination.

--- resources/views/vendor/pagination/tailwind.blade.php
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -1,10 +1,10 @@
 @if($paginator->hasPages())
 <nav class="flex justify-center" role="navigation">
     <!-- Prev link -->
-    @if($pagination->onFisrtPage())
+    @if($paginator->onFirstPage())
     <span class="px-4 py-2 bg-gray-300 text-gray-500 rounded-l-lg">Previous</span>
     @else
-    <a href="{{ $pagination->previousPageUrl() }}"
+    <a href="{{ $paginator->previousPageUrl() }}"
         class="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-l-lg">Previous</a>
     @endif
 
@@ -17,7 +17,7 @@
     <!-- Array Of Links -->
     @if(is_array($element))
     @foreach($element as $page => $url)
-    @if($page == $pagination->currentPage())
+    @if($page == $paginator->currentPage())
     <span class="px-4 py-2 bg-blue-500 text-white">{{ $page }}</span>
     @else
     <a href="{{ $url }}" class="px-4 py-2 bg-gray-200 text-gray-700 hover:bg-blug-600 hover:text-white">{{ $page }}</a>
@@ -25,6 +25,15 @@
     @endforeach
     @endif
     @endforeach
+
+    <!-- Next link -->
+    @if($paginator->hasMorePages())
+    <a href="{{ $paginator->nextPageUrl() }}"
+        class="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-r-lg">Next</a>
+    @else
+    <span class="px-4 py-2 bg-gray-300 text-gray-500 rounded-r-lg">Next</span>
+
+    @endif
 </nav>
 @else
 
